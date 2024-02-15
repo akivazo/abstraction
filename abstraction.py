fca44375bf4027f92e4593f777ce40a3ea805389
Akiva Zonenfeld <akivazo1@gmail.com>
2024-02-15 11:17:21 +0200

    simpler

--- abstraction.py
+++ abstraction.py
@@ -1,61 +1,64 @@
 from .node import node, directed_edge
-from .event import event, eof_event
+from .event import event, eof_event, event_supplier, event_supplier_impl
 from typing import List, Mapping, Callable, Set
 from weakref import WeakSet
 
 
 class abstraction:
-    def __init__(self, event_supplier :Callable[[], event]):
+    def __init__(self, event_supplier : event_supplier):
         self.__nodes: Set[node] = WeakSet()
         self.__event_map: Mapping[event, node] = {}
-        self.__event_supplier: Callable[[], event] = event_supplier
+        self.__event_supplier = event_supplier
         self.__last_node = None
+        self.__next_layer_event_supplier = event_supplier_impl()
+        self.__next_layer: abstraction = None
 
 
-    def get_event_intentsity(self, event: event):
+    def __get_event_intentsity(self, event: event):
         return event.intensity
 
-    def create_node(self, event: event):
-        new_node = node(intensity=self.get_event_intentsity(event), event=event)
+    def __create_node(self, event: event):
+        new_node = node(intensity=self.__get_event_intentsity(event), event=event)
         self.__nodes.add(new_node)
         self.__event_map[event] = new_node
         self.__last_node = new_node
         return new_node
 
-    def get_last_node_created(self):
+    def __get_last_node_created(self):
         return self.__last_node
 
-    def add_new_leaf_node(self, node: node):
-        pass
-
-    def get_node_intensity(self, node: node):
+    def __get_node_intensity(self, node: node):
         return node.get_intensity()
 
-    def connect_to_node(self, node_to_connetct_to: node, node: node, connection_strngth):
-        new_edge = directed_edge(source=node, target=node_to_connetct_to, strength=connection_strngth)
-        node.add_edge(new_edge)
-
-    def is_part_of_branch(self, node: node):
+    def __is_part_of_branch(self, node: node):
         return node.get_rank() == 1
     
     def __merge_nodes(self, node1: node, node2: node):
         next_node = node2.get_edge(0).next
         new_event = node1.get_event() + node2.get_event()
-        new_node = self.create_node(new_event)
+        new_node = self.__create_node(new_event)
         new_node.connect_to_node(next_node, strengrh=node1.get_intensity() + node2.get_intensity())
         return new_node
 
+    def __propigate_to_next_layer(self, event):
+        try:
+            self.__next_layer_event_supplier.add_event(event)
+        except Exception as e:
+            raise e
+            self.__next_layer = abstraction(self.__next_layer_event_supplier)
+
     def __find_branch(self, node: node):
         branch_node = None
-        if self.is_part_of_branch(node):
+        if self.__is_part_of_branch(node):
             branch_node = node
             next_node = node.get_edge(0).next
             rest_of_branch_node = self.__find_branch(next_node)
             if rest_of_branch_node is not None:
                 branch_node = self.__merge_nodes(node, rest_of_branch_node)
+                self.__propigate_to_next_layer(branch_node.get_event())
         return branch_node
     
-    def summarize_isolated_branches(self):
+    def __summarize_isolated_branches(self):
         def aux(branches_parts_nodes: list):
             try:
                 part_of_branch_node = branches_parts_nodes.pop(0)
@@ -64,7 +67,7 @@ class abstraction:
             branch_node = self.__find_branch(part_of_branch_node)
             return [branch_node] + aux(branches_parts_nodes)
 
-        branches_parts_nodes = list(filter(self.is_part_of_branch, self.__nodes))
+        branches_parts_nodes = list(filter(self.__is_part_of_branch, self.__nodes))
         branches_nodes: List[node] = aux(branches_parts_nodes)
         self.__nodes.union(branches_nodes)
         for branch_node in branches_nodes:
@@ -74,18 +77,18 @@ class abstraction:
         new_node: node
         try:
             new_node = self.__event_map[event]
-            new_node.increase_intensity(self.get_event_intentsity(event))
+            new_node.increase_intensity(self.__get_event_intentsity(event))
         except KeyError:
-            new_node = self.create_node(event)
-        last_node = self.get_last_node_created()
+            new_node = self.__create_node(event)
+        last_node = self.__get_last_node_created()
         if last_node is not None:
-            last_node.connect_to_node(node=new_node, strengrh=self.get_node_intensity(new_node))
+            last_node.connect_to_node(node=new_node, strengrh=self.__get_node_intensity(new_node))
 
     def learn(self):
         while True:
             new_event = self.__event_supplier()
             if isinstance(new_event, eof_event) or new_event is None:
-                self.summarize_isolated_branches()
+                self.__summarize_isolated_branches()
                 break
             self.process(new_event)
         
--- event.py
+++ event.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
-from typing import List, Mapping
+from typing import Any, List, Mapping
 from abc import abstractmethod, ABC
+
 
 @dataclass
 class event(ABC):
@@ -8,12 +9,30 @@ class event(ABC):
 
     def __post_init__(self):
         if self.intensity < 1:
-            raise ValueError("intensity must be graeter than 0 integer")
+            raise ValueError("intensity must be graeter than 0")
 
         
     def __add__(self, other: "event"):
         return event(self.intensity + other.intensity)
 
+class event_supplier(ABC):
+    @abstractmethod
+    def __call__(self) -> event:
+        pass
+
+class event_supplier_impl(event_supplier):
+    def __init__(self) -> None:
+        self.__events_queue = []
+
+    def add_event(self, event: event):
+        self.__events_queue.append(event)
+
+    def __call__(self) -> Any:
+        try:
+            event = self.__events_queue.pop()
+            return event
+        except IndexError:
+            return None
 @dataclass
 class eof_event:
     pass
